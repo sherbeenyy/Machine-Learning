a842a058838a5f1572d809c59630699ee68affe7
Ahmed Hesham <A.Ahmed06380@student.aast.edu>
2024-06-30 09:36:27 +0300

    fixed some stuff

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program
 {
-      static void Main(string[] args) {
+      static void Main() {
         double[][] features =  [
             [ 1.0, 2.0 ],
             [ 2.0, 3.0 ],
@@ -9,14 +9,12 @@
             [ 7.0, 7.0 ],
             [ 8.0, 7.0 ]
         ];
-        int[] labels = [ 0, 0, 0, 1, 1, 1 ]; 
+        int[] labels = [ 0, 0, 0, 1, 1, 1 ];
 
-        // Initialize KNN with K=3
         KNN knn = new KNN(3);
         knn.Train(features, labels);
 
-        // Predict the class of a new data point
-        double[] newPoint = [ 5.0, 5.0 ];
+        double[] newPoint = [ 2.0, 1.0 ];
         int predictedClass = knn.Predict(newPoint);
 
         Console.WriteLine($"Predicted class for the new data point [{string.Join(", ", newPoint)}]: {predictedClass}");
--- obj/Debug/net8.0/Machine-Learning.AssemblyInfo.cs
+++ obj/Debug/net8.0/Machine-Learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Machine-Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b64a43e3900fba6debc7d1ddd46b563b4a79a29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Machine-Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Machine-Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
